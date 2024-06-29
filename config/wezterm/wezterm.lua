744bb87a483aa309a6dc99207a48c2d046b314a5
dozken <dosmukhamed@gmail.com>
2024-06-29 14:08:35 +0500

    wezterm conf update

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This table will hold the configuration.
 local config = {}
@@ -7,41 +7,47 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-    config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
-
 
 local custom = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
 -- custom.background = "#000000"
 config.color_schemes = {
-    ["OLEDppuccin"] = custom,
+	["OLEDppuccin"] = custom,
 }
 -- config.color_scheme = "OLEDppuccin"
 
-config.window_decorations = 'RESIZE'
+-- config.window_decorations = "NONE"
 config.hide_tab_bar_if_only_one_tab = true
+config.font = wezterm.font("JetBrains Mono")
 config.font_size = 16
 config.window_background_opacity = 0.9
 config.macos_window_background_blur = 10
-config.native_macos_fullscreen_mode = true
+config.window_padding = {
+	left = 2,
+	right = 2,
+	-- top = 2,
+	bottom = 0,
+}
+-- config.enable_hyperlinks = true
+-- config.native_macos_fullscreen_mode = true
 
+-- config.default_gui_startup_args = { "fullscreen" }
 local act = wezterm.action
 config.keys = {}
 for i = 1, 9 do
-    local keyEntry = {
-        key = tostring(i),
-        mods = 'CMD',
-        action = act.Multiple {
-            act.SendKey { key = 'a', mods = 'CTRL' },
-            act.SendKey { key = tostring(i) },
-        },
-        when = {
-            app = "tmux",
-        },
-    }
-    table.insert(config.keys, keyEntry)
+	local keyEntry = {
+		key = tostring(i),
+		mods = "CMD",
+		action = act.Multiple({
+			act.SendKey({ key = "a", mods = "CTRL" }),
+			act.SendKey({ key = tostring(i) }),
+		}),
+		when = {
+			app = "tmux",
+		},
+	}
+	table.insert(config.keys, keyEntry)
 end
 
-
 return config
-
